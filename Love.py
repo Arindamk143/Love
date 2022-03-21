d4d0ab7c40aa688760d4aeabed6f3cb4375481ab
Arindamk143 <102083962+Arindamk143@users.noreply.github.com>
2022-03-22 02:54:44 +0530

    Hi

---
+++ Love.py
@@ -0,0 +1,52 @@
+'''Dairy Of Mine And My cute Love Puja'''
+
+Hi = '❤'
+for i in range(1,4):
+    print(i*Hi)
+
+class Love:
+    Parcentage = '100%'
+    def Heart(self):
+        print(f"Love IS Life For Us {self.Parcentage}, {self.WeAre}")
+    
+    @staticmethod
+    def Time():
+        print("This Time is Valentine Day")
+    
+    @staticmethod
+    def Promise():
+        print("I alwas Love with you, Never Leave You Alone")
+    
+    @staticmethod
+    def Duo():
+        print("I LOVE YOU MY DEAR PUJA")
+
+    @staticmethod
+    def Feelings():
+        print("I MISS YOU DEAR PUJA")
+    
+    @staticmethod
+    def Moments():
+        print("I KISS YOU PUJA")
+
+
+
+Me = Love()
+Me.WeAre = 'Puja+Arindam'
+Me.Heart()
+Me.Time()
+Me.Promise()
+Me.Duo()
+Me.Feelings()
+Me.Moments()
+
+Mix = int(input("Enter A Number:\n"))
+if Mix == 143:
+    print("True Love")
+else:
+    print("False Love")
+joy = '❤'
+for i in range(1,4):
+    print(i*joy)
+
+'''BYE BYE , END OF STORY'''
